7558860617cf882e7c994319c546257a7d6ad834
Arpita Das <arpitad@live.unc.edu>
2020-11-05 01:53:58 -0500

    video background successful

--- my-app/src/pages/FirstPage.js
+++ my-app/src/pages/FirstPage.js
@@ -2,20 +2,23 @@ import 'bulma/css/bulma.css';
 import './../styles/FirstPage.css';
 import renderLogo from './../logo/logo.js';
 import login from '../components/login';
-import './bg.mp4';
+import bg from './bg.mp4';
 
 export default function firstPage() {
     const videosource = 'bg.mp4';
     return <div>
-        <section className="hero yellow is-fullheight">
-            <div className="hero-body logo">
-                <div className="svg-container column is-7">
-                    {renderLogo()}
+            <video id="myVideo" autoPlay loop muted>
+                <source src={bg} type='video/mp4' />
+            </video>
+            <section className="hero is-fullheight">
+                <div className="hero-body logo">
+                    <div className="svg-container column is-7">
+                        {renderLogo()}
+                    </div>
+                    <div className="column is-one-third">
+                        {login()}
+                    </div>
                 </div>
-                <div className="column is-one-third">
-                    {login()}
-                </div>
-            </div>
-        </section>
-    </div>
+            </section>
+        </div>
 }
