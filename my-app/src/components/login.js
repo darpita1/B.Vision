3f7012b9284020a2d896cfc752824671e75d5068
Arpita Das <arpitad@live.unc.edu>
2020-11-07 20:07:42 -0500

    one more react login modification

--- my-app/src/components/login.js
+++ my-app/src/components/login.js
@@ -8,35 +8,38 @@ class Login extends Component {
             username: '',
             password: ''
         };
+
+        this.handleSubmit = this.handleSubmit.bind(this);   // only necessary if using a function without () so maybe not necessary in this case
     }
     render() {
         return (<div className="login-container">
-                <form className="loginform">
+                <form className="loginform" onSubmit={this.handleSubmit}>
                     <h1 className="title">Login</h1>
                     <div className="field">
                         <label className="label">Username</label>
                         <div className="control">
-                            <input className="input" type="text" placeholder="Username" name="username" onChange={(event,newValue) => this.setState({username:newValue})}></input>
+                            <input className="input" type="text" placeholder="Username" value={this.state.username} name="username" onChange={(event) => this.setState({username:event.target.value})}></input>
                         </div>
                     </div>
                     <div className="field">
                         <label className="label">Password</label>
                         <div className="control">
-                            <input className="input" type="password" placeholder="Password" name="password" onChange={(event, newValue) => this.setState({password:newValue})}></input>
+                            <input className="input" type="password" placeholder="Password" value={this.state.password} name="password" onChange={(event) => this.setState({password:event.target.value})}></input>
                         </div>
                     </div>
                     <div className="field">
                         <div className="control">
-                            <input className="button is-dark" value="Create Account" type="submit" onClick={(event) => this.handleClick(event)}/>
-                            <input className="button is-dark" type="submit"/>
+                            <input className="button is-dark" value="Create Account" type="submit" />
+                            <input className="button is-dark" type="submit" onClick={(event) => this.handleSubmit(event)}/>
                         </div>
                     </div>
                 </form>
             </div>);
     }
 
-    handleClick() {
-        // logic for handling authorization 
+    handleSubmit() {
+        // logic for handling authorization
+        console.log(this.state);
     }
 }
 
