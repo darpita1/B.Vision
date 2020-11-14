f5c0c7964fbd07bd88ec7801d91418fb8c640eee
tejaspruthi <45113417+tejaspruthi@users.noreply.github.com>
2020-11-14 01:09:29 -0500

    main page stuff

--- my-app/src/pages/mainpage.js
+++ my-app/src/pages/mainpage.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import VideoContainer from './videocontainer.js';
 import VideoSelector from './videoselector.js';
+import Searchbar from './searchbar.js';
 
 function Mainpage() {
     return (
         <div>
-            {/* <Searchbar /> */}
+            <Searchbar />
             <VideoContainer />
             <VideoSelector />
         </div>
