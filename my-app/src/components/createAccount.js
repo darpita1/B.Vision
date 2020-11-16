035d59e82a0ec3ae5a81580eb17c3df68e62097c
jsjk77 <54329776+jsjk77@users.noreply.github.com>
2020-11-15 23:52:12 -0500

    Revert "Merge branch 'main' of https://github.com/darpita1/B.Vision into main"
    This reverts commit 0afcae740271e87032ee5eb67a79608dd6896fa9, reversing
    changes made to d3e1866cec96d33efa00769914ea159bab326e06.

--- my-app/src/components/createAccount.js
+++ my-app/src/components/createAccount.js
@@ -1,97 +1,82 @@
+import React, { Component } from 'react';
 import 'bulma/css/bulma.css';
 import '../styles/createAccount.css';
-import React, { useRef, useState } from 'react';
-import { useAuth } from '../contexts/AuthContext';
-import { Link } from "react-router-dom";
 
-export default function CreateAccount() {
-    
-    const usernameRef = useRef();
-    const passwordRef = useRef();
-    const nameRef = useRef();
-    const ageRef = useRef();
-    const[title, setTitle] = useState();
-    const { signup } = useAuth();
-
-    const [error, setError] = useState("");
-    const [loading, setLoading] = useState(false);
-    function handleRadio(event) {
-        //console.log(event.target.value);
-        setTitle(event.target.value);
-    }
-    async function handleSubmit(e) {
-        e.preventDefault();
-
-        try {
-            console.log(usernameRef.current.value);
-            console.log(passwordRef.current.value);
-            console.log("title:", title);
-            setError("");
-            setLoading(true);
-            let a = await signup(usernameRef.current.value, passwordRef.current.value);
-            // another function that will handle the data and put it into the database
-        } catch {
-            setError("Account already exists ");
+class CreateAccount extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            username: '',
+            title: '',
+            password: '',
+            age: ''
         }
-        
-        setLoading(false);
+        this.handleSubmit =this.handleSubmit.bind(this);
     }
 
-    return (<div className="create-account-container container">
-    <form className="accountform" onSubmit={handleSubmit}>
-        <h1 className="title">Create Account</h1>
-        <div className="field">
-            <label className="label">Name</label>
-            <div className="control">
-                <input className="input" type="text" placeholder="Name" ref={nameRef} name="name"></input>
+    render() {
+        return (<div className="create-account-container container">
+        <form className="accountform" onSubmit={this.handleSubmit}>
+            <h1 className="title">Create Account</h1>
+            <div className="field">
+                <label className="label">Name</label>
+                <div className="control">
+                    <input className="input" type="text" placeholder="Name" value={this.state.name} name="name" onChange={(e) => this.setState({name:e.target.value})}></input>
+                    {this.state.name}
+                </div>
             </div>
-        </div>
-        <div className="field">
-            <label className="label">Username</label>
-            <div className="control">
-                <input className="input" type="text" placeholder="Username" ref={usernameRef} name="username"></input>
+            <div className="field">
+                <label className="label">Username</label>
+                <div className="control">
+                    <input className="input" type="text" placeholder="Username" value={this.state.username} name="username" onChange={(e) => this.setState({username:e.target.value})}></input>
+                    {this.state.username}
+                </div>
             </div>
-        </div>
-        <div className="field">
-            <label className="label">Password</label>
-            <div className="control">
-                <input className="input" type="password" placeholder="Password" ref={passwordRef} name="password"></input>
+            <div className="field">
+                <label className="label">Password</label>
+                <div className="control">
+                    <input className="input" type="password" placeholder="Password" value={this.state.password} name="password" onChange={(e) => this.setState({password:e.target.value})}></input>
+                    {this.state.password}
+                </div>
             </div>
-        </div>
-        <div className="field">
-            <label className="label">Age</label>
-            <div className="control">
-                <input className="input" type="text" placeholder="Age" ref={ageRef} name="age"></input>
+            <div className="field">
+                <label className="label">Age</label>
+                <div className="control">
+                    <input className="input" type="text" placeholder="Age" value={this.state.age} name="age" onChange={(event) => this.setState({age:event.target.value})}></input>
+                    {this.state.age}
+                </div>
             </div>
-        </div>
-        <div className="title-radio">
-        <label className="label">Title</label>
-            <div className="control" onChange={handleRadio}>
-                <label className="radio">
-                    <input type="radio" name="answer" value="Teacher"/>
-                            Teacher
-                </label>
-                <label className="radio">
-                    <input type="radio" name="answer" value="Student"/>
-                            Student
-                </label>
-                <label className="radio">
-                    <input type="radio" name="answer" value="Choreographer"/>
-                            Choreographer
-                </label>
+            <div className="title-radio">
+            <label className="label">Title</label>
+                <div className="control">
+                    <label className="radio">
+                        <input type="radio" name="answer"/>
+                             Teacher
+                    </label>
+                    <label className="radio">
+                        <input type="radio" name="answer"/>
+                             Student
+                    </label>
+                    <label className="radio">
+                        <input type="radio" name="answer"/>
+                             Choreographer
+                    </label>
+                </div>
             </div>
-        </div>
-        <br/>
-        {error && <h2>{error}</h2>}
-        <div className="field">
-            <div className="control">
-                <input className="button is-dark" type="submit" disabled={loading}/>
+            <hr/>
+            <div className="field">
+                <div className="control">
+                    <input className="button is-dark" type="submit" onClick={(event) => this.handleSubmit(event)}/>
+                </div>
             </div>
-        </div>
-    </form>
-    <hr/>
-    <div>
-        Already have an Account? <Link to="/login">Log In</Link>
-    </div>
-</div>);
+        </form>
+    </div>)
+    }
+
+    handleSubmit() {
+        console.log("submit");
+    }
 }
+
+export default CreateAccount;
