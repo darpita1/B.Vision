411ec9a173d1519be95094292d8aecdcea359082
Arpita Das <arpitad@live.unc.edu>
2020-11-23 18:05:46 -0500

    test delete

--- my-app/src/components/uservideos.js
+++ my-app/src/components/uservideos.js
@@ -16,29 +16,38 @@ function Uservideos(props) {
             }).then((x) => setvids(x.data.videos));
             setLoading(true);
         }
-        
-        
-          
-
-        
     }
-    
+
+    async function deleteVid(e) {
+        let vid = e.target.id;
+        console.log(vids);
+        const result = await axios({
+            method: 'delete',
+            url: `https://b-vision-18af8.firebaseio.com/users/${props.username}/videos/0.json`,
+            withCredentials: true
+        });
+        console.log(vids);
+    }
     readviddata();
-    console.log(vids);
+    //console.log("before", vids);
 
     return (
-        
        <div>
-
-        <h1> videos </h1>
+           <br/>
+        <h1 className="title"> My Videos </h1>
         {vids.map((vid) =>{
+            if (vid != 'google.com'){
            return (
-            <li>
-            <a href = {vid}> link </a>
-            </li>
-           )
-           
-           
+            <div>
+                <li key={vid}>
+                    <a className="btn btn-link" href = {vid}> Video </a>
+                    <button id={vid} className="button is-danger is-small is-outlined" onClick={(e) => deleteVid(e)}>
+                        <span>Delete</span>
+                    </button>
+                </li>
+                <br/>
+            </div>
+           )}
         })}
 
        </div>
