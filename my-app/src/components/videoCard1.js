0697f110813685666bef626bf95511bd24d354e8
tejaspruthi <45113417+tejaspruthi@users.noreply.github.com>
2020-11-23 11:39:40 -0500

    videocards 1-3

---
+++ my-app/src/components/videoCard1.js
@@ -0,0 +1,36 @@
+import React, {useState} from 'react';
+import 'bulma/css/bulma.css';
+import image1 from '../media/bg_createAccount.JPG';
+
+
+function VideoCard1() {
+
+    const [one, setone] = useState("https://tutorials.avideo.com/view/img/video-placeholder-gray.png");
+
+    
+    // call to database to fill options maybe
+
+    function changeImage(e) {
+        let newUrl1 = `https://firebasestorage.googleapis.com/v0/b/b-vision-18af8.appspot.com/o/${e}.png?alt=media`;
+        let newUrl = `https://firebasestorage.googleapis.com/v0/b/b-vision-18af8.appspot.com/o/thumbnails%2F${e}.jpg?alt=media&token=50019ba2-f751-4904-ad74-e6628dc7c89b`
+        setone(newUrl);
+    }
+
+    return (
+        <div className="card">
+            <div className="card-content container">
+                <h2 className="subtitle is-4">Video 1</h2>
+                <img src= {one} alt="bvis" width="500px" height="300px"/>
+            </div>
+            <div className="card-footer">
+            <select onChange={(e)=> changeImage(e.target.value)}>
+                    
+                    <option value="fast_jhummar">Fast Jhummar</option>
+                    <option value="jhummar_var1">Jhummar Variation</option>
+                    
+                </select>
+            </div>
+        </div> );
+}
+
+export default VideoCard1;
